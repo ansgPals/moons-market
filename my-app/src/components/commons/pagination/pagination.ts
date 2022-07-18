f6f346d5051b0d7cc67493c75219ed8d4c40113f
ansgPals <iamm71kr@gmail.com>
2022-07-19 02:00:25 +0900

    7/19

--- my-app/src/components/commons/pagination/pagination.ts
+++
@@ -1,9 +0,0 @@
-export interface IPaginationPageProps {
-  startPage: number;
-  pickPage: number;
-  lastPage: number;
-  OnclickPage: (pageNumber: number) => () => void;
-  OnClickPrevPage: () => void;
-
-  OnClickNextPage: () => void;
-}
